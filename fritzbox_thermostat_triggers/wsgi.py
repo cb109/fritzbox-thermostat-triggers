408f38df4a6a2b98a0777df64649f1ae2a725b0f
Christoph Buelter <buelter.christoph@gmail.com>
2022-02-27 11:50:23 +0100

    Initial commit

---
+++ fritzbox_thermostat_triggers/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fritzbox_thermostat_triggers project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fritzbox_thermostat_triggers.settings')
+
+application = get_wsgi_application()
