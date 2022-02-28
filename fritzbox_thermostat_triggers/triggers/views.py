005d74ebc3d88373e63cce25f664f538d7eba9a6
Christoph Buelter <buelter.christoph@gmail.com>
2022-02-28 22:50:02 +0100

    List triggers on page

--- fritzbox_thermostat_triggers/triggers/views.py
+++ fritzbox_thermostat_triggers/triggers/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from fritzbox_thermostat_triggers.triggers.models import Trigger
+
+
+@login_required
+def triggers(request):
+    triggers = Trigger.objects.filter(triggered=False).order_by(
+        "thermostat__name", "temperature"
+    )
+    return render(request, "triggers/triggers.html", {"triggers": triggers})
