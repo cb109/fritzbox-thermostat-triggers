6c3c9bdc39e041c7352f1d4c32a51dd385b1089f
Christoph Buelter <buelter.christoph@gmail.com>
2022-02-28 23:31:12 +0100

    List onetime and recurring triggers separately

--- fritzbox_thermostat_triggers/triggers/views.py
+++ fritzbox_thermostat_triggers/triggers/views.py
@@ -9,4 +9,13 @@ def triggers(request):
     triggers = Trigger.objects.filter(enabled=True).order_by(
         "thermostat__name", "temperature"
     )
-    return render(request, "triggers/triggers.html", {"triggers": triggers})
+    onetime_triggers = [trigger for trigger in triggers if not trigger.recurring]
+    recurring_triggers = [trigger for trigger in triggers if trigger.recurring]
+    return render(
+        request,
+        "triggers/triggers.html",
+        {
+            "onetime_triggers": onetime_triggers,
+            "recurring_triggers": recurring_triggers,
+        },
+    )
