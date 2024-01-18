2602ce76756e2c4c93b5900f883b88af96dae3c6
Christoph Buelter <buelter.christoph@gmail.com>
2024-01-18 21:19:36 +0100

    Make the switch toggle enabled of the trigger
    No html update yet, just an ajax call, otherwise we'd lose that nice
    switch animation.

--- fritzbox_thermostat_triggers/triggers/views.py
+++ fritzbox_thermostat_triggers/triggers/views.py
@@ -1,13 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from django.http import HttpResponse
 
 from fritzbox_thermostat_triggers.triggers.models import Trigger
 
 
 @login_required
-def triggers(request):
-    triggers = Trigger.objects.filter(enabled=True).order_by(
-        "thermostat__name", "temperature"
+@require_http_methods(("GET",))
+def list_triggers(request):
+    triggers = Trigger.objects.order_by(
+        "time", "thermostat__name", "temperature"
     )
     onetime_triggers = [trigger for trigger in triggers if not trigger.recurring]
     recurring_triggers = [trigger for trigger in triggers if trigger.recurring]
@@ -19,3 +22,25 @@ def triggers(request):
             "recurring_triggers": recurring_triggers,
         },
     )
+
+
+@login_required
+@require_http_methods(("GET",))
+def trigger_card(request, pk: int):
+    trigger = Trigger.objects.get(id=pk)
+    return render(
+        request,
+        "triggers/_trigger_card.html",
+        {
+            "trigger": trigger,
+        },
+    )
+
+
+@login_required
+@require_http_methods(("POST",))
+def toggle_trigger(request, pk: int):
+    trigger = Trigger.objects.get(id=pk)
+    trigger.enabled = not trigger.enabled
+    trigger.save(update_fields=["enabled"])
+    return HttpResponse()
--- fritzbox_thermostat_triggers/urls.py
+++ fritzbox_thermostat_triggers/urls.py
@@ -5,7 +5,9 @@ from django.views.generic import RedirectView
 from fritzbox_thermostat_triggers.triggers import views
 
 urlpatterns = [
-    path("triggers/", views.triggers, name="triggers"),
     path("admin/", admin.site.urls),
+    path("triggers/", views.list_triggers),
+    path("trigger/<int:pk>/card", views.trigger_card),
+    path("trigger/<int:pk>/toggle", views.toggle_trigger),
     path("", RedirectView.as_view(url="triggers/")),
 ]
